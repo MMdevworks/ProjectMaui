35d86b3d89a564ade46310654e6fb3a3888006c1
Marie <mmdevq@gmail.com>
2024-12-14 19:40:40 -0800

    edit and delete fx

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -6,9 +6,11 @@ using ProjectMaui.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ProjectMaui.ViewModels
 {
@@ -28,17 +30,39 @@ namespace ProjectMaui.ViewModels
         [ObservableProperty]
         private string mobile;
 
-        //[ObservableProperty]
-        //private bool isEdit;
+        [ObservableProperty]
+        private string notes;
+
+        [ObservableProperty]
+        private bool isFormVisible;
+        [ObservableProperty]
+        private bool isAddBtnVisible = true;
+        [ObservableProperty]
+        private bool isEdit;
 
         private Client selectedClient;
 
         public ObservableCollection<Client> Clients { get; } = new();
 
+        public ICommand EditCommand => new RelayCommand<Client>(EditClient);
+        public ICommand DeleteCommand => new RelayCommand<Client>(async (client) => await DeleteClient(client));
+
         public MainViewModel(LocalDbService localDbService)
         {
             this.localService = localDbService;
             Task.Run(LoadClientsAsync);
+        }
+
+        [RelayCommand]
+        private void AddClient()
+        {
+            IsFormVisible = true;
+            IsAddBtnVisible = false;
+            IsEdit = false;
+            Name = string.Empty;
+            Email = string.Empty;
+            Mobile = string.Empty; 
+            Notes = string.Empty;
         }
 
         [RelayCommand]
@@ -60,32 +84,31 @@ namespace ProjectMaui.ViewModels
             try
             {
                 IsBusy = true;
-                if (ClientId == 0) // create
+                if (IsEdit) //edit
+                {
+                    await localService.UpdateClient(new Client
+                    {
+                        Id = selectedClient.Id,
+                        Name = Name,
+                        Email = Email,
+                        Mobile = Mobile,
+                        Notes = Notes
+                    });
+                }
+                else //add
                 {
                     await localService.CreateClient(new Client
                     {
                         Name = Name,
                         Email = Email,
-                        Mobile = Mobile
+                        Mobile = Mobile,
+                        Notes = Notes
                     });
                 }
-                else
-                {
-                    await localService.UpdateClient(new Client //else edit by id
-                    {
-                        Id = ClientId,
-                        Name = Name,
-                        Email = Email,
-                        Mobile = Mobile
-                    });
 
-                    ClientId = 0;
-                }
-                //clear fields
-                Name = string.Empty;
-                Email = string.Empty;
-                Mobile = string.Empty;
-                //reload clients
+                // hide form and reload
+                IsFormVisible = false;
+                IsAddBtnVisible = true;
                 await LoadClientsAsync();
             }
             finally
@@ -105,24 +128,29 @@ namespace ProjectMaui.ViewModels
             }
         }
 
-        [RelayCommand]
+        //[RelayCommand]
         private void EditClient(Client client)
         {
             if (client != null)
             {
-                ClientId = client.Id;
+                Debug.WriteLine($"===================>  Editing client: {client.Name}");
+                selectedClient = client;
+                IsFormVisible = true;
+                IsAddBtnVisible = false;
+                IsEdit = true;
                 Name = client.Name;
                 Email = client.Email;
                 Mobile = client.Mobile;
-                //IsEdit = true;
+                Notes = client.Notes;
             }
         }
 
-        [RelayCommand]
+        //[RelayCommand]
         private async Task DeleteClient(Client client)
         {
             if (client != null)
             {
+                Debug.WriteLine($"===================>  Deleting client: {client.Name}");
                 bool userConfirm = await App.Current.MainPage.DisplayAlert(
                     "Delete Client",$"Are you sure you want to delete {client.Name}?",
                     "Yes",
